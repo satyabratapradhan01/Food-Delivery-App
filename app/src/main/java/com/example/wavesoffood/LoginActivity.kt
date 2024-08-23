b93336bd071774b5fea85610c5d137b8fd3dd8c5
satyabratapradhan <satyabratapradhann@gmail.com>
2024-08-24 00:49:43 +0530

    create singup page using xml

--- app/src/main/java/com/example/wavesoffood/LoginActivity.kt
+++ app/src/main/java/com/example/wavesoffood/LoginActivity.kt
@@ -1,21 +1,32 @@
 package com.example.wavesoffood
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.wavesoffood.databinding.ActivityStartBinding
+
+class SignActivity : AppCompatActivity() {
+    // Add necessary code for SignActivity if needed
+}
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, singupActivity::class.java)
+            startActivity(intent)
+        }
+        binding.dontHaveButton.setOnClickListener {
+            val intent = Intent(this, singupActivity::class.java)
+            startActivity(intent)
         }
     }
 }
+
 
